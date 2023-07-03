b5b229942a3e7017bd030938220e95732bf006c0
igor47g0 <Edihigor17@gmail.com>
2023-07-03 19:52:07 -0300

    ajutes

---
+++ Dilhero  1/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entre = new Scanner(System.in);
+        int fome, dinheiro;
+        System.out.println("Esta com fome ?"
+          + "\n1- sim"+"\n2- Não");
+         dinheiro = entre.nextInt();
+        System.out.println("Tem dinheiro ?"
+                + "\n1- sim"+"\n2- Não");
+        fome  = entre.nextInt();
+         if (fome == 1 && dinheiro == 1){
+             System.out.println("Va para a cantina!");
+         }else if (fome == 1&& dinheiro==2){
+             System.out.println("Trabalho que é bom nada");
+         } else if (fome==2 && dinheiro==1 ) {
+             System.out.println("Garde o dinheiro");
+         } else if (fome == 2&& dinheiro==2) {
+             System.out.println("Va caçar o que fazer!");
+         }else{
+             System.out.println("Invalido!");
+         }
+    }
+    }
